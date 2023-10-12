a07c6cb0bbbcb82ba4d830cf8fc7e19e3e3c6100
ZanY22 <cebureksteam@gmail.com>
2023-10-12 19:56:27 +0300

    ScoreText

--- Assets/Scripts/Bird.cs
+++ Assets/Scripts/Bird.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
 
 public class Bird : MonoBehaviour
 {
+    int Score;
+    public TMP_Text ScoreText;
     public float rotatePower;
     public float jumpSpeed;
     Rigidbody2D rb;
@@ -18,5 +22,21 @@ public class Bird : MonoBehaviour
             rb.velocity = Vector2.up * jumpSpeed;
         }
             transform.eulerAngles = new Vector3(0, 0, rb.velocity.y * rotatePower);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Die();
+    }
+    void Die()
+    {
+        var sceneName = SceneManager.GetActiveScene().name;
+        SceneManager.LoadScene(sceneName);
+
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Score++;
+        ScoreText.text = Score.ToString();
     }
 }
